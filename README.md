77c1cf7d7fa6ec541a937468168f8ec69010520e
Khanh Du <khanhdu1609@gmail.com>
2024-04-29 09:28:24 +0700

    add another files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-![alt text](image.png)
+# Student card processing
+## 1.Read the image
+![alt text](<Processed Images/z5393034160252_8edb2fa38b6bea0b262bb837aa68da53.jpg>)
+## 2.Smoothing the card by erosion and dilation algorithm
+![alt text](<Processed Images/z5393035503928_53d4184a8ef9c2ef900fb6999398acaa.jpg>)
+## 3.Get the boundaries of the card by using Houghline
+![alt text](<Processed Images/z5393036149344_0f659dbe4990f8bdba5d7d5c0c7751e1.jpg>)
+## 4. Get 4 intersecting points of 4 lines. The points is presented as blue points
+![alt text](<Processed Images/z5393036784636_50bb84ac06f4aacffd9c62f0e1caf7ba.jpg>)
+## 5. Using affine transform to crop the student card from image
+![alt text](<Processed Images/z5393037158412_1427bc3387cdcb7427277ceb9b4c66b6.jpg>)
+## 6. Specify the coordinate of each information field in the cropped image
+![alt text](<Processed Images/z5393082283274_b10f0cab5e727e3c58ff9dc4dfdce6e7.jpg>) ![alt text](<Processed Images/z5393082766725_1cf2eeedf3874ab2155a84e3f2d03c4d.jpg>) ![alt text](<Processed Images/z5393083206754_03e630616005dcad4b1bfdadbe319505.jpg>) ![alt text](<Processed Images/z5393083649764_dd130eb7b5ee601d5bd1f9bd0d2cd446.jpg>) ![alt text](<Processed Images/z5393084032414_b759c0e736157602b1aaf88a4ee3af19.jpg>) ![alt text](<Processed Images/z5393084432962_b4c4cfaa2c012d955d50af9993fc19ca.jpg>) ![alt text](<Processed Images/z5393085209453_c155f2eb5bba93a11184e4fdc7a77092.jpg>)
+## 7. Use vietocr model to read the informations
+![alt text](<Processed Images/z5393037580020_95508a4a697439d2265d280d88d4b618.jpg>)
---
+++ deploy.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from ExtractStudentInfo import ExtractStudentInfo
+from vietocr.tool.predictor import Predictor
+from vietocr.tool.config import Cfg
+from PIL import Image
+
+extractor = ExtractStudentInfo()
+if __name__ == '__main__':
+    # Load and preprocess the image
+    img_ori = cv2.imread('data_test/1 (11).jpg')
+    img= cv2.resize(img_ori, (600, 500))
+    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img_smooth= extractor.smoothing(img_gray, extractor.kernel)
+
+    #Find the boundaries
+    img_edge = extractor.findEdge(img_smooth)
+    lines = extractor.findLines(img_edge)
+    # Find lines using Hough transform or any other method
+    # Draw the lines on the edge image
+    line_img = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2BGR)
+    for line in lines:
+        rho, theta = line
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a * rho
+        y0 = b * rho
+        x1 = int(x0 + 1000 * (-b))
+        y1 = int(y0 + 1000 * (a))
+        x2 = int(x0 - 1000 * (-b))
+        y2 = int(y0 - 1000 * (a))
+        cv2.line(line_img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    # Find intersection points
+    intersection_points = extractor.point_inter(lines)
+
+    # Draw the intersection points on the original image
+    for point in intersection_points:
+        cv2.circle(img, (int(point[0]), int(point[1])), 5, (255, 0, 0), -1)
+    
+    #Flatten image
+    scan = extractor.flatten(img, intersection_points)
+    scan = cv2.cvtColor(scan, cv2.COLOR_BGR2RGB)
+
+    #Crop the informations
+    info_img = extractor.cropInfo(scan)
+
+    #Read the informations
+    informations = extractor.readInfo(info_img)
+
+    print(informations)
+
